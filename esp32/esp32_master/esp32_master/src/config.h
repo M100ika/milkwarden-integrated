d5686e1c5adca46c28d6e7239b26f0ee7380b3d1
M100ika <maxat.suieubayev@gmail.com>
2026-05-10 12:46:53 +0500

    as

--- esp32/esp32_master/esp32_master/src/config.h
+++ esp32/esp32_master/esp32_master/src/config.h
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 // ─── Firmware ─────────────────────────────────────────────────────────────────
-#define FIRMWARE_VERSION        "1.1.3"
+#define FIRMWARE_VERSION        "1.1.4"
 
 // ─── WiFi STA (connect to router) ─────────────────────────────────────────────
 struct WifiCredential { const char* ssid; const char* password; };
