f7197be5fb44d7cb9cc303085e295c2141d941e1
M100ika <maxat.suieubayev@gmail.com>
2026-05-15 13:20:28 +0500

    1.4.6_1

--- esp32/esp32_milkwarden/src/config.h
+++ esp32/esp32_milkwarden/src/config.h
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 // ─── Firmware ─────────────────────────────────────────────────────────────────
-#define FIRMWARE_VERSION            "1.4.6"
+#define FIRMWARE_VERSION            "1.4.6.1"
 
 // ─── Device Identity ──────────────────────────────────────────────────────────
 #define ESP_DEVICE_ID               1          // Slave number 1-4
